e3d1767edcc9949578176bc7e7865f290ca46314
Rohan <rohan26ir@gmail.com>
2025-08-18 16:11:46 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -9,6 +9,44 @@ const port= process.env.PORT || "5000";
 app.use(cors());
 app.use(express.json());
 
+//user:   backpractice
+//pass:   IqpRX1Tc0zio2GHk
+
+// paste full coped code here and add user and pass in the code
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.8c67l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = "mongodb+srv://backpractice:IqpRX1Tc0zio2GHk@cluster0.8c67l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 
 // CRUD
 app.get("/", (req, res) => {
